f43f470c4b9345549fbeef2a9ded2806ecce77a4
yeob <maintain98@naver.com>
2024-11-01 15:33:28 +0900

    cc

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import TOC from './components/Toc';
-import Subject from './components/Subject';
-import ReadContent from './components/ReadContent';
-import Control from './components/Control';
-import CreateContent from './components/CreateContent';
  
 function App() {
- 
-  const [mode, setMode] = useState('create');
 
-  const [selected_content_id, setSelected_content_id] = useState(1);
- 
-  const [state, setState] = useState({
-    subject : {title:'WEB', sub:'World Wide Web'},
-    welcome:{title:'welcome', desc:'Hello, React.'},
-    contents : [
-      {id:1, title:'HTML', desc:'HTML is HyperText ...'},
-      {id:2, title:'CSS', desc:'CSS is For design ...'},
-      {id:3, title:'JavaScript', desc:'JavaScript is interactive ...'}
-    ]
-  }); 
-
-  var _title;
-  var _desc;
-  var _article;
 
   return (
     <div className="App">
