74847a61525474b3ed4b13a39f681ce593ac2a8b
yeob <maintain98@naver.com>
2024-11-01 16:13:55 +0900

    aaa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import './App.css';
- 
+
 function App() {
 
+  const goToExternalUrl = (url) => {
+    window.location.href = url;
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            HI
+          </p>
+          <div style={{ marginTop: '20px' }}>
+            <button onClick={() => goToExternalUrl('https://swaplinkie.xyz/api/gateway/test')}>
+              Go to swaplinkie API
+            </button>
+            <button onClick={() => goToExternalUrl('http://127.0.0.1:8000/gateway/test')}>
+              Go to Local API
+            </button>
+          </div>
+        </header>
+      </div>
   );
 }
 
